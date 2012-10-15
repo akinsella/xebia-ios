3fcca334e24c7eed23989127c378a6e3e220a95f
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-10-16 00:59:23 +0200

    Add support for Twitter authenticated request: Give higher X-Rate for twitter requests

--- nodejs-xebia-mobile-proxy/application/app.js
+++ nodejs-xebia-mobile-proxy/application/app.js
@@ -11,6 +11,8 @@ var mysql = require('mysql');
 var underscore = require("underscore");
 var cf = require("cloudfoundry");
 
+var twitter = require('ntwitter');
+
 var EXPIRE_CACHE = true;
 var USE_CACHE = true;
 
@@ -83,6 +85,14 @@ if(!cf.app) {
 }
 
 var app = express.createServer();
+
+
+var twit = new twitter({
+  consumer_key: process.env.OAUTH_TWITTER_CONSUMER_KEY,
+  consumer_secret:  process.env.OAUTH_TWITTER_CONSUMER_SECRET,
+  access_token_key:  process.env.OAUTH_TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.OAUTH_TWITTER_ACCESS_TOKEN_SECRET
+});
 
 var redisConfig = cf.services["redis-2.2"][0];
 //var mysqlConfig = cf.services["mysql-5.1"][0];
@@ -308,11 +318,130 @@ app.get('/index.html', function(req, res) {
     res.sendfile(__dirname + '/www/index.html');
 });
 
+
+twit.stream('user', {track:'XebiaFr'}, function(stream) {
+//twit.stream('statuses/filter', { track: ['XebiaFR'] }, function(stream) {
+
+  var xebiaFrTweets = [];
+
+  stream.on('data', function (data) {
+    console.log(data);
+
+  var tweet = data;
+
+
+  if (xebiaFrTweets.length > 100) {
+      xebiaFrTweets.splice(100);
+  }
+
+  xebiaFrTweets.unshift(shortenTweet(tweet));
+
+  });
+
+  stream.on('end', function (response) {
+    // Handle a disconnection
+      console.log("Twitter Stream Connection End: " + response);
+  });
+
+  stream.on('destroy', function (response) {
+    // Handle a 'silent' disconnection from Twitter, no end/error event fired
+    console.log("Twitter Stream Connection destroyed: " + response);
+  });
+
+});
+
 //app.get('/twitter/:user', function(req, res) {
+app.get('/' + API_VERSION + '/twitter/auth/stream/XebiaFr', function(req, res) {
+    var callback = getParameterByName(req.url, 'callback');
+    res.send(callback ? callback + "(" + JSON.stringify(xebiaFrTweets) + ");" : JSON.stringify(xebiaFrTweets));
+});
+
+
+//app.get('/twitter/:user', function(req, res) {
+function shortenTweet(tweet) {
+    var tweetShortened = {
+        id:tweet.id,
+        id_str:tweet.id_str,
+        created_at:tweet.created_at,
+        text:tweet.text,
+        favorited:tweet.favorited,
+        retweeted:tweet.retweeted,
+        retweet_count:tweet.retweet_count,
+        entities:tweet.entities
+    };
+
+    if (tweet.user) {
+        tweetShortened.user = {
+            id:tweet.user.id,
+            id_str:tweet.user.id_str,
+            screen_name:tweet.user.screen_name,
+            name:tweet.user.name,
+            profile_image_url:tweet.user.profile_image_url
+        };
+    }
+
+    if (tweet.retweeted_status) {
+        tweetShortened.retweeted_status = {
+            id:tweet.retweeted_status.id,
+            id_str:tweet.retweeted_status.id_str,
+            created_at:tweet.retweeted_status.created_at
+        };
+
+        if (tweet.retweeted_status.user) {
+            tweetShortened.retweeted_status.user = {
+                id:String(tweet.retweeted_status.user.id),
+                screen_name:tweet.retweeted_status.user.screen_name,
+                name:tweet.retweeted_status.user.name,
+                profile_image_url:tweet.retweeted_status.user.profile_image_url
+            };
+        }
+
+    }
+    return tweetShortened;
+}
+app.get('/' + API_VERSION + '/twitter/auth/user/:user', function(req, res) {
+
+    var user = req.params.user;
+    console.log("User: " + user);
+    var callback = getParameterByName(req.url, 'callback');
+
+    redisClient.get(req.url, function (err, data) {
+        if (!err && data) {
+            console.log("[" + req.url + "] A reply is in cache key: '" + getCacheKey(req) + "', returning immediatly the reply");
+            responseData(200, "", data,  {  callback: callback, req: req, res : res });
+        }
+        else {
+            console.log("[" + req.url + "] No cached reply found for key: '" + getCacheKey(req) + "'");
+            twit.getUserTimeline("screen_name=" + user + "&contributor_details=false&include_entities=true&include_rts=true&exclude_replies=false&count=50&exclude_replies=false",
+                function(error, data) {
+                    if (error) {
+                        var errorMessage = err.name + ": " + err.message;
+                        responseData(500, errorMessage, undefined,  {  callback: callback, req: req, res : res });
+                    }
+                    else {
+                        var tweets = data;
+
+                        var tweetsShortened = [];
+
+                        _(tweets).each(function(tweet) {
+                            tweetsShortened.push(shortenTweet(tweet));
+                        });
+
+                        var jsonData = JSON.stringify(tweetsShortened);
+
+                        redisClient.set(getCacheKey(req), jsonData);
+                        redisClient.expire(getCacheKey(req), 60 * 60);
+                        console.log("[" + req.url + "] Fetched Response from url: " + jsonData);
+                        callback(200, "", jsonData, {  callback: callback, req: req, res : res });
+                    }
+                });
+        }
+    });
+});
+
 app.get('/' + API_VERSION + '/twitter/user/:user', function(req, res) {
 
     var user = req.params.user;
-//    var user = "XebiaFR";
     console.log("User: " + user);
     var twitterUrl = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + user + "&contributor_details=false&include_entities=true&include_rts=true&exclude_replies=false&count=50&exclude_replies=false";
     console.log("Twitter Url: " + twitterUrl);
@@ -321,7 +450,7 @@ app.get('/' + API_VERSION + '/twitter/user/:user', function(req, res) {
         req: req,
         res: res,
         url: twitterUrl,
-        cacheKey: req.url,
+        cacheKey: getCacheKey(req),
         forceNoCache: false,//getIfUseCache(req),
         callback: onTwitterDataLoaded,
         user: user,
@@ -347,46 +476,8 @@ app.get('/' + API_VERSION + '/twitter/user/:user', function(req, res) {
             var tweetsShortened = [];
 
             _(JSON.parse(tweets)).each(function(tweet) {
-                var tweetShortened = {
-                    id: tweet.id,
-                    id_str: tweet.id_str,
-                    created_at: tweet.created_at,
-                    text: tweet.text,
-                    favorited: tweet.favorited,
-                    retweeted: tweet.retweeted,
-                    retweet_count: tweet.retweet_count,
-                    entities: tweet.entities
-                };
 
-                if (tweet.user) {
-                    tweetShortened.user =  {
-                        id: tweet.user.id,
-                        id_str: tweet.user.id_str,
-                        screen_name: tweet.user.screen_name,
-                        name: tweet.user.name,
-                        profile_image_url: tweet.user.profile_image_url
-                    };
-                }
-
-                if (tweet.retweeted_status) {
-                    tweetShortened.retweeted_status = {
-                         id: tweet.retweeted_status.id,
-                         id_str: tweet.retweeted_status.id_str,
-                         created_at: tweet.retweeted_status.created_at
-                    };
-
-                    if (tweet.retweeted_status.user) {
-                        tweetShortened.retweeted_status.user =  {
-                            id: String(tweet.retweeted_status.user.id),
-                            screen_name: tweet.retweeted_status.user.screen_name,
-                            name: tweet.retweeted_status.user.name,
-                            profile_image_url: tweet.retweeted_status.user.profile_image_url
-                        };
-                    }
-
-                }
-
-                tweetsShortened.push(tweetShortened);
+                tweetsShortened.push(shortenTweet(tweet));
             });
 
             res.send(callback ? callback + "(" + JSON.stringify(tweetsShortened) + ");" : JSON.stringify(tweetsShortened));
@@ -395,6 +486,45 @@ app.get('/' + API_VERSION + '/twitter/user/:user', function(req, res) {
 
 });
 
+function shortenEvent(event) {
+    var shortenedEvent = {
+        id:String(event.id),
+        type:"eventbrite",
+        category:event.category,
+        capacity:event.capacity,
+        title:event.title,
+        start_date:event.start_date,
+        end_date:event.end_date,
+        timezone_offset:event.timezone_offset,
+        tags:event.tags,
+        created:event.created,
+        url:event.url,
+        privacy:event.privacy,
+        status:event.status,
+        description:event.description,
+        description_plain_text:event.description.replace(/(<([^>]+)>)/ig, ""),
+        organizer:{
+            url:event.organizer.url,
+            description:event.organizer.description,
+            id:String(event.organizer.id),
+            name:event.organizer.name
+        },
+        venue:{
+            city:event.venue.city,
+            name:event.venue.name,
+            country:event.venue.country,
+            region:event.venue.region,
+            longitude:event.venue.longitude,
+            postal_code:event.venue.postal_code,
+            address_2:event.venue.address_2,
+            address:event.venue.address,
+            latitude:event.venue.latitude,
+            country_code:event.venue.country_code,
+            id:String(event.venue.id)
+        }
+    };
+    return shortenedEvent;
+}
 app.get('/' + API_VERSION + '/event/list', function(req, res) {
 
     var eventbriteUrl = "https://www.eventbrite.com/json/event_search?app_key=EHHWMU473LTVEO4JFY&organizer=xebia";
@@ -404,7 +534,7 @@ app.get('/' + API_VERSION + '/event/list', function(req, res) {
         req: req,
         res: res,
         url: eventbriteUrl,
-        cacheKey: req.url,
+        cacheKey: getCacheKey(req),
         forceNoCache: false,
         callback: onEventbriteDataLoaded,
         cacheTimeout: 300,
@@ -429,43 +559,7 @@ app.get('/' + API_VERSION + '/event/list', function(req, res) {
             var shortenedEvents = [];
 
                 _(_.pluck(_.tail(JSON.parse(events).events), "event")).each(function(event) {
-                var shortenedEvent = {
-                    id: String(event.id),
-                    type: "eventbrite",
-                    category:event.category,
-                    capacity: event.capacity,
-                    title: event.title,
-                    start_date: event.start_date,
-                    end_date: event.end_date,
-                    timezone_offset: event.timezone_offset,
-                    tags: event.tags,
-                    created: event.created,
-                    url: event.url,
-                    privacy: event.privacy,
-                    status: event.status,
-                    description: event.description,
-                    description_plain_text: event.description.replace(/(<([^>]+)>)/ig,""),
-                    organizer: {
-                        url: event.organizer.url,
-                        description: event.organizer.description,
-                        id: String(event.organizer.id),
-                        name: event.organizer.name
-                    },
-                    venue: {
-                        city: event.venue.city,
-                        name: event.venue.name,
-                        country: event.venue.country,
-                        region: event.venue.region,
-                        longitude: event.venue.longitude,
-                        postal_code: event.venue.postal_code,
-                        address_2: event.venue.address_2,
-                        address: event.venue.address,
-                        latitude: event.venue.latitude,
-                        country_code: event.venue.country_code,
-                        id: String(event.venue.id)
-                    }
-                };
-                shortenedEvents.push(shortenedEvent);
+                shortenedEvents.push(shortenEvent(event));
             });
 
             res.send(callback ? callback + "(" + JSON.stringify(shortenedEvents) + ");" : JSON.stringify(shortenedEvents));
